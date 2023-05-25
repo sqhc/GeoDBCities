95f49469691009fa01bb1a620bca27c12d61bd24
shenqing <shenqin1@msu.edu>
2023-05-25 13:06:19 -0400

    Initial Commit

---
+++ GeoDBCities/GeoDBCitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoDBCitiesApp.swift
+//  GeoDBCities
+//
+//  Created by 沈清昊 on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct GeoDBCitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
